831a117529e32d7cc3a96145021be8d074bc6b35
fre-ben <fschocke@gmail.com>
2021-05-18 12:14:32 +0200

    Initalize project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Tarzan",
+      animal: "Cat",
+      breed: "Giercat",
+    }),
+    React.createElement(Pet, {
+      name: "Ekelmann",
+      animal: "Bird",
+      breed: "Dove",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
