8f378a738137ec6df174ec153cf8de049ba7d978
fre-ben <fschocke@gmail.com>
2021-05-21 11:10:08 +0200

    💄 Style App.js with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ const App = () => {
 
   return (
     <ThemeContext.Provider value={theme}>
-      <div>
+      <div
+        className="p-0 m-0"
+        style={{
+          background:
+            "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
+        }}
+      >
         <Router>
-          <header>
-            <Link to="/">
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link to="/" className="text-6xl text-white hover:text-gray-200">
               <h1>Adopt Me!</h1>
             </Link>
           </header>
